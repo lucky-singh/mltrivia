0f799579c74db2b190012da750be50e8634bb105
lucky-singh <luckysingh223@gmail.com>
2019-04-02 19:57:58 +0530

    Initial commit

---
+++ ML_Trivia/lambda/us-east-1_serverlessrepo-ml-trivia-acloudgurualexa-1WZ1KAB4799OC/questions.js
@@ -0,0 +1,100 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'What is not a regression': [
+        'It relates inputs to outputs',
+        'It is used for prediction',
+      ],
+    },
+    {
+      'What happens in clustering?': [
+        'Inputs are divided into groups',
+        'Inputs are multiplied',
+      ],
+    },
+    {
+      'What is the "Curse of Dimensionality?': [
+        'The difficulty of searching through a solution space becomes much harder as you have more features.',
+        'The difficulty of searching through a solution space becomes much harder as you have less features',
+      ],
+    },
+    {
+      'What is true about decision trees?': [
+        'Decision trees are prone to be overfit',
+        'Decision trees are hard to interpret',
+      ],
+    },
+    {
+      'Although machine learning is an interesting concept, there are limited business applications in which it is useful. ': [
+        'True',
+        'False',
+      ],
+    },
+    {
+      'How does deep learning differ from conventional machine learning?': [
+        'Deep learning algorithms can handle more data and run with less supervision from data scientists.',
+        'There are no real differences between the two -- they are the same tool with different names.',
+      ],
+    },
+    {
+      'Machine learning applications are too complex to run in the cloud.': [
+        'False',
+        'True',
+      ],
+    },
+    {
+      'Which of the following is a technique frequently used in machine learning and AI programs?': [
+        'Classification of data into categories based on attributes.',
+        'Data munging',
+      ],
+    },
+    {
+      'Bias is error due to overly simplistic assumptions in the learning algorithm you’re using. This can lead to the model underfitting your data, making it hard for it to have high predictive accuracy and for you to generalize your knowledge from the training set to the test set.': [
+        'True',
+        'False',
+      ],
+    },
+    {
+      'Variance is error due to too much complexity in the learning algorithm you’re using. This leads to the algorithm being highly sensitive to high degrees of variation in your training data, which can lead your model to overfit the data.': [
+        'True',
+        'False',
+      ],
+    },
+    {
+      'Variance is error due to overly simplistic assumptions in the learning algorithm you’re using. This can lead to the model underfitting your data, making it hard for it to have high predictive accuracy and for you to generalize your knowledge from the training set to the test set.': [
+        'False',
+        'True',
+      ],
+    },
+    {
+      'Bias is error due to too much complexity in the learning algorithm you’re using. This leads to the algorithm being highly sensitive to high degrees of variation in your training data, which can lead your model to overfit the data.': [
+        'False',
+        'True',
+      ],
+    },
+    {
+      'Supervised learning requires training labeled data': [
+        'True',
+        'False',
+      ],
+    },
+    {
+      'Which of the machine learning algorithm can be used for imputing missing values of both categorical and continuous variables?': [
+        'K-NN',
+        'Logistic Regression',
+      ],
+    },
+    {
+      'Which of the them are true about Manhattan distance?': [
+        'It can be used for continuous variables',
+        'It can be used for categorical variables',
+      ],
+    },
+  ],
+};
